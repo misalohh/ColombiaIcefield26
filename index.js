d30836c01ee9d37591b75863d4f02752f89a9080
Mia Harvey-Hill <mia@Mias-MacBook-Air-4.local>
2026-08-21 14:34:07 +0100

    Basic Shape

---
+++ index.js
@@ -0,0 +1,27 @@
+import * as THREE from 'three';
+
+const width = window.innerWidth;
+const height = window.innerHeight;
+
+const renderer = new THREE.WebGLRenderer({ antialias: true });
+renderer.setSize(width, height);
+document.body.appendChild(renderer.domElement);
+
+const fov = 75;
+const aspect = width/height;
+const near = 0.1;
+const far = 10;
+const camera = new THREE.PerspectiveCamera(fov, aspect, near, far);
+camera.position.z = 2;
+const scene = new THREE.Scene();
+
+const geo = new THREE.IcosahedronGeometry(1.0, 2);
+const mat = new THREE.MeshBasicMaterial({
+    color: 0xccff
+});
+const mesh = new THREE.Mesh(geo, mat);
+scene.add(mesh);
+
+renderer.render(scene, camera);
+
+
